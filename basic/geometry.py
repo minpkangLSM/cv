d84a2409210f0ab5833953555f574b5a16c18d4a
minpkang <minpkang@gmail.com>
2022-05-26 00:47:31 +0900

    chapter2. day5

--- basic/geometry.py
+++ basic/geometry.py
@@ -23,12 +23,19 @@ def rot(theta):
 def scale(sx, sy):
     return np.array([[sx, 0, 1], [0, sy, 1], [0, 0, 1]])
 
-def bi_inter(coord):
+def inrange(coord, shape):
+    return coord[0]>=0 and coord[0]<shape[1] and coord[1]>=0 and coord[1]<shape[0]
+
+def bi_inter(coord, shape):
 
     upper_left = np.array([math.floor(coord[0]), math.floor(coord[1])])
     upper_right = np.array([math.floor(coord[0]), math.ceil(coord[1])])
     bottom_left = np.array([math.ceil(coord[0]), math.floor(coord[1])])
     bottom_right = np.array([math.ceil(coord[0]), math.ceil(coord[1])])
+    if not inrange(upper_left, shape) : upper_left = coord.astype(np.int16)
+    if not inrange(upper_right, shape) : upper_right = coord.astype(np.int16)
+    if not inrange(bottom_left, shape) : bottom_left = coord.astype(np.int16)
+    if not inrange(bottom_right, shape) : bottom_right = coord.astype(np.int16)
 
     return upper_left, upper_right, bottom_left, bottom_right
 
@@ -36,12 +43,14 @@ def rotation(file_dir,
              theta,
              x_resize=0.5,
              y_resize=0.5,
-             center=True):
+             center=True,
+             interp=True):
     """
     rotate image theta,
     :param file_dir: image directory
     :param theta: unit - degree
     :param center: rotate image in origin(=False) or center(=True)
+    :param interp: binary interpolation
     :return:
     """
     img = cv2.imread(file_dir, cv2.IMREAD_COLOR)
@@ -72,19 +81,20 @@ def rotation(file_dir,
     coord_homo = np.stack([x_coord.flatten(),
                            y_coord.flatten(),
                            np.ones_like(x_coord.flatten())], axis=0).astype(np.int64)
-    source_coord = np.dot(lin.inv(h_matrix), coord_homo).astype(np.int64)
+    source_coord = np.dot(lin.inv(h_matrix), coord_homo)
 
     for i, j in zip(source_coord.transpose(1,0), coord_homo.transpose(1,0)):
-        #i[0] : x, i[1] : y, || frame shape : (y, x, z)
         if i[0]>=0 and i[0]<frame.shape[1] and i[1]>=0 and i[1]<frame.shape[0]:
-            # ul, ur, bl, br = bi_inter(i, img.shape)
-            # alpha = i[0]-ul[0]
-            # beta = i[1]-ul[1]
-            # interp_1 = (1-alpha) * img[ul[1]][ul[0]][:] + alpha * img[ur[1]][ur[0]][:]
-            # interp_2 = (1-alpha) * img[bl[1]][bl[0]][:] + alpha * img[br[1]][br[0]][:]
-            # interp_3 = (1-beta) * interp_1 + beta * interp_2
-            # frame[j[1]][j[0]][:] = interp_3
-            frame[j[1]][j[0]][:] = img[i[1]][i[0]][:]
+            if interp :
+                ul, ur, bl, br = bi_inter(i, img.shape)
+                alpha = i[0]-ul[0]
+                beta = i[1]-ul[1]
+                interp_1 = (1-alpha) * img[ul[1]][ul[0]][:] + alpha * img[ur[1]][ur[0]][:]
+                interp_2 = (1-alpha) * img[bl[1]][bl[0]][:] + alpha * img[br[1]][br[0]][:]
+                interp_3 = (1-beta) * interp_1 + beta * interp_2
+                frame[j[1]][j[0]][:] = interp_3
+            else :
+                frame[j[1]][j[0]][:] = img[i[1].astype(int)][i[0].astype(int)][:]
 
     plt.subplot(121)
     plt.imshow(img)
@@ -92,6 +102,7 @@ def rotation(file_dir,
     plt.imshow(frame)
     plt.show()
 
-file_dir = "D:/cv/data/prac/KakaoTalk_20220518_215457616.jpg"
+file_dir = "D:/cv/data/prac/KakaoTalk_20220518_215457616_01.jpg"
 rotation(file_dir=file_dir,
-         theta=-30)
+         theta=10,
+         interp=False)
