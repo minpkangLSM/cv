d638b6df5ee325f8ebd77694fcb5e7cc2d31ed89
minpkang <minpkang@gmail.com>
2022-09-08 01:16:28 +0900

    SIFT BASIC FROM LOWE 1999, 2004 - 4. Accurate Keypoint Localization HARRIS CORNER

--- SIFT/parts/scale_space.py
+++ SIFT/parts/scale_space.py
@@ -7,7 +7,7 @@ import math
 import copy
 import numpy as np
 import numba as nb
-from numba import jit, njit, uint8, int64
+from numba import jit, njit, uint8, int16, int64, float64
 from basic.filters import *
 from basic.geometry import *
 from time import process_time
@@ -19,9 +19,9 @@ class feature :
 
     @staticmethod
     def scaleSpace(img,
-                     s,
-                     octaveNum,
-                     sigma=1.6):
+                   s,
+                   octaveNum,
+                   sigma=1.6):
        """
        the number of images in the stack of blurred images for each octave : s+3
        :param img: image type = gray scale
@@ -208,37 +208,215 @@ class feature :
         return None
 
     @staticmethod
-    @jit (uint8[:,:,:](uint8[:,:,:]))
+    def extractExtremum(dogSpace):
+        """
+        order) scale space -> dog -> extremum
+        select the candidate points from scale space from feature.scaleSpace function.
+        :param scaleSpace:
+        :param sigmas:
+        :return:
+        """
+        dogIdx = dogSpace.keys()
+        extremum = {}
+
+        # STEP 1) extract sample points(of extremum)
+        """
+        dog : scale space에 대하여 difference를 수행한 scale space
+        extremum : dog scale space 상에서, 극점을 갖는 위치를 갖는 변수
+        """
+        for idx in dogIdx:
+            octave = dogSpace[idx]
+            extreBox = feature.__extremumSub(octave)
+            extIdx = np.where(extreBox == 1)
+            extremum[idx] = extIdx
+
+        return extremum
+
+    @staticmethod
+    @jit(uint8[:, :, :](uint8[:, :, :]))
     def __extremumSub(octave):
         """
         SUB function for locate extremum in DOG space
         :param octave:
         :return:
         """
-
         boolBox = np.ones_like(octave)
         extrBox = np.zeros_like(octave).astype(uint8)
-
-        for x in range(1, boolBox.shape[1]-1):
-            for y in range(1, boolBox.shape[0]-1):
-                for z in range(1, boolBox.shape[2]-1):
-
-                    if boolBox[y, x, z]==0 : continue
-
+        for x in range(1, boolBox.shape[1] - 1):
+            for y in range(1, boolBox.shape[0] - 1):
+                for z in range(1, boolBox.shape[2] - 1):
+                    if boolBox[y, x, z] == 0: continue
                     dVal = octave[y, x, z]
-                    sample = octave[y-1:y+2, x-1:x+2, z-1:z+2]
-                    results = np.sum((dVal>sample).flatten())-1
-                    if results==0 or results==26 :
-                        boolBox[y-1:y+2, x-1:x+2, z-1:z+2] = 0
+                    sample = octave[y - 1:y + 2, x - 1:x + 2, z - 1:z + 2]
+                    results = np.sum((dVal > sample).flatten()) - 1
+                    if results == 0 or results == 26:
+                        boolBox[y - 1:y + 2, x - 1:x + 2, z - 1:z + 2] = 0
                         extrBox[y, x, z] = 1
-
         return extrBox
+
+    @staticmethod
+    def localization(dogSpace,
+                     extremum,
+                     offsetThr,
+                     contrastThr):
+
+        # STEP 2) extract feature points
+        """
+        1) calculate Gradient / Heissan for extremum points
+        2) interploate the location of extremum which have value under 0.5
+        3) discard the location of extremum which have D(hat x) under contrast threshold
+        """
+        dogIdx = dogSpace.keys()
+        localizedExtremum = {}
+
+        for idx in dogIdx:
+            # dog scale space 상 octave 순서대로 진행
+            """1. derivate Y"""
+            octaveY = dogSpace[idx]
+            dy = sobelHeightAxis(octaveY)  # shape (Y, X, Z)
+            dy2 = sobelHeightAxis(dy)  # shape (Y, X, Z)
+            dyx = sobelWidthAxis(dy)  # shape (Y, X, Z)
+            """2. derivate X"""
+            octaveX = np.transpose(octaveY, (1, 0, 2))
+            dx = sobelHeightAxis(octaveX)  # shape (X, Y, Z)
+            dx2 = sobelHeightAxis(dx)  # shape (X, Y, Z)
+            dxT = np.transpose(dx, (2, 0, 1))  # shape (Z, X, Y)
+            dxz = sobelHeightAxis(dxT)  # shape (Z, X, Y)
+            # (Y, X, Z) 순서로 모양 맞추기
+            dx = np.transpose(dx, (1, 0, 2))
+            dx2 = np.transpose(dx2, (1, 0, 2))  # shape (Y, X, Z)
+            dxz = np.transpose(dxz, (2, 1, 0))  # shape (Y, X, Z)
+            """3. derivate Z"""
+            octaveZ = np.transpose(octaveY, (2, 0, 1))
+            dz = sobelHeightAxis(octaveZ)  # shape (Z, Y, X)
+            dz2 = sobelHeightAxis(dz)  # shape (Z, Y, X)
+            dzy = sobelWidthAxis(dz)  # shape (Z, Y, X)
+            # (Y, X, Z) 순서로 모양 맞추기
+            dz = np.transpose(dz, (1, 2, 0))
+            dz2 = np.transpose(dz2, (1, 2, 0))  # shape (Y, X, Z)
+            dzy = np.transpose(dzy, (1, 2, 0))  # shape (Y, X, Z)
+
+            # gradient \frac{dD}{dX}
+            samplePoints = extremum[idx]
+            dDdx = dx[samplePoints][:, np.newaxis]
+            dDdy = dy[samplePoints][:, np.newaxis]
+            dDdz = dz[samplePoints][:, np.newaxis]
+            gradient = np.concatenate([dDdx, dDdy, dDdz], axis=1)[:, :, np.newaxis]
+            # hessian \frac{d^2D}{dX^2}
+            dDdx2 = dx2[samplePoints][np.newaxis, :]
+            dDdxz = dxz[samplePoints][np.newaxis, :]
+            dDdyx = dyx[samplePoints][np.newaxis, :]
+            dDdy2 = dy2[samplePoints][np.newaxis, :]
+            dDdzy = dzy[samplePoints][np.newaxis, :]
+            dDdz2 = dz2[samplePoints][np.newaxis, :]
+            hessianTmp = np.concatenate([dDdx2, dDdyx, dDdxz, dDdyx, dDdy2, dDdzy, dDdxz, dDdzy, dDdz2], axis=0)[:, :,
+                         np.newaxis]
+            hessian = hessianTmp.reshape((-1, 3, 3))
+            """interpolate extremum location"""
+            XhatVal = np.matmul(hessian / 255, gradient / 255)  # normalize value in range 0 ~ 1
+            interpolatedTmp = copy.deepcopy(extremum[idx])
+            for no, val in enumerate(XhatVal):
+                if any(abs(val)) >= offsetThr:
+                    val = val.flatten()
+                    interpolatedTmp[0][no] = interpolatedTmp[0][no] - val[0]
+                    interpolatedTmp[1][no] = interpolatedTmp[1][no] - val[1]
+                    interpolatedTmp[2][no] = interpolatedTmp[2][no] - val[2]
+            """remove low contrast value"""
+            iTmp = np.concatenate((interpolatedTmp[0][:, np.newaxis],
+                                   interpolatedTmp[1][:, np.newaxis],
+                                   interpolatedTmp[2][:, np.newaxis]), axis=-1)  # (N, 3)
+            iTmp = iTmp[:, :, np.newaxis]  # (N, 3, 1)
+            gradientRshp = gradient.transpose((0, 2, 1))
+            contrastVal = dogSpace[idx][extremum[idx]] / 255 + 0.5 * np.matmul(gradientRshp / 255, iTmp).flatten()
+            contrastValIdx = np.where((contrastVal >= contrastThr) | (contrastVal <= -contrastThr))
+            # discard value under contrastThr
+            localizedExtremum[idx] = (extremum[idx][0][contrastValIdx],
+                                      extremum[idx][1][contrastValIdx],
+                                      extremum[idx][2][contrastValIdx])
+        return localizedExtremum
+
+    @staticmethod
+    def edgeRemover(dogSpace,
+                    extremum,
+                    sigmaX,
+                    sigmaY,
+                    r):
+        """
+        해당 단계는 피쳐(feature, keypoint)에 대한 localization이 끝난 후,
+        edge에 존재하는 피쳐포인트를 제거하기 위한 단계로 Harris corner를 사용하여 edge 상에 존재하는 피쳐를 제거한다.
+        :param dogSpace:
+        :param extremum:
+        :param r:
+        :return:
+        """
+        # remove features on the edge using Harris Corner
+        extremumOctaveIdx = extremum.keys() # dog space의 모든 옥타브를 조사할 필요는 없고, feature가 존재하는 옥타브에 대해서만 진행하면 됨
+        ratio = (r+1)*(r+1)/r
+        features = {}
+        # calculate derivation for each octave
+        for idx in extremumOctaveIdx :
+            dogNorm = dogSpace[idx]/255.
+            # calculate derivation for earch layer
+            dy = sobelHeightAxis(dogNorm,
+                                 ddepth=cv2.CV_64F)
+            dx = sobelWidthAxis(dogNorm,
+                                ddepth=cv2.CV_64F)
+            gdy2 = gaussian(img=dy*dy,
+                            sigmaX=sigmaX,
+                            sigmaY=sigmaY)
+            gdx2 = gaussian(img=dx*dx,
+                            sigmaX=sigmaX,
+                            sigmaY=sigmaY)
+            gdyx = gaussian(img=dy*dx,
+                            sigmaX=sigmaX,
+                            sigmaY=sigmaY)
+            # harris corner
+            ey = extremum[idx][0]
+            ex = extremum[idx][1]
+            ez = extremum[idx][2]
+            isCorner = feature.__harrisCorner(locY=ey,
+                                              locX=ex,
+                                              locZ=ez,
+                                              gdy2=gdy2,
+                                              gdx2=gdx2,
+                                              gdyx=gdyx,
+                                              ratio=ratio)
+            cornerY = ey[isCorner.astype(bool)]
+            cornerX = ex[isCorner.astype(bool)]
+            cornerZ = ez[isCorner.astype(bool)]
+            features[idx] = (cornerY,
+                             cornerX,
+                             cornerZ)
+
+        return features
+
+    @staticmethod
+    @jit((int64[:])(int64[:], int64[:], int64[:], float64[:,:,:], float64[:,:,:], float64[:,:,:], float64))
+    def __harrisCorner(locY,
+                       locX,
+                       locZ,
+                       gdy2,
+                       gdx2,
+                       gdyx,
+                       ratio):
+        isCorner = np.zeros_like(locY.astype(int64))
+        idx = 0
+        for y, x, z in zip(locY, locX, locZ):
+            A = np.array([[gdy2[y,x,z], gdyx[y,x,z]],
+                          [gdyx[y,x,z], gdx2[y,x,z]]])
+            trace = A[0,0] + A[1,1]
+            det = A[0,0]*A[1,1] - A[1,0]*A[0,1]
+            if det < 0 : continue
+            elif trace*trace/(det+1e-4) <= ratio : isCorner[idx]=1
+            idx+=1
+        return isCorner
 
 if __name__ == "__main__":
 
-    imgDir = "D:\\cv\\data\\prac\\blacktriangle.png"
+    imgDir = "D:\\cv\\data\\prac\\cannytest.png"
     img = cv2.imread(imgDir, cv2.IMREAD_GRAYSCALE) # Height(Y), Width(X)
-    img = cv2.resize(img, (150, 100)) # cv2.resize input shape order ( X(Width), Y(Height))
+    img = cv2.resize(img, (250, 200)) # cv2.resize input shape order ( X(Width), Y(Height))
+    # img shape order Y, X (height, width)
     img = img
     deg = 0
     img_rot = rotation(img=img,
@@ -252,48 +430,54 @@ if __name__ == "__main__":
     # DoG space
     DoG = feature.dog(ss)
     # Get extremum
-    ex = feature.extremum(dogSpace=DoG,
-                          offsetThr=0.5,
-                          contrastThr=0.03)
+    naiveEx = feature.extractExtremum(dogSpace=DoG)
+    # accurate keypoint localization
+    localizedEx = feature.localization(dogSpace=DoG,
+                                       extremum=naiveEx,
+                                       offsetThr=0.5,
+                                       contrastThr=0.03)
+    features = feature.edgeRemover(dogSpace=DoG,
+                                   extremum=localizedEx,
+                                   sigmaX=1.5,
+                                   sigmaY=1.5,
+                                   r=10)
     t2 = process_time()
     print("Process time of Chapter 3 : ", t2 - t1)
-    print(ex)
-    # fig, ax = plt.subplots()
-    # ax.imshow(img, cmap='gray')
-    #
-    # for idx in ex.keys():
-    #     # if idx!=0 : continue
-    #     x_list = []
-    #     y_list = []
-    #
-    #     # deg rotation -> 그냥 로테이션 원상복귀만 시키는 파트 (scale과는 아무 관련 없음)
-    #     # X, Y, Z 순서 주의할 것 -> 영상은 기본적으로 (Height, Width, Channel) = (Y, X, Z) 순서임
-    #     shift = tran(-img.shape[1]/2, -img.shape[0]/2) # Homogeneous matrix는 x, y 순서로 받아야 한다.
-    #     shift_rev = tran(img.shape[1]/2, img.shape[0]/2)
-    #     rot_matrix = rot(deg)
-    #     h_matrix = np.linalg.multi_dot([shift_rev, rot_matrix, shift])
-    #
-    #     coord_homo = np.stack([ex[idx][1],
-    #                            ex[idx][0],
-    #                            np.ones_like(ex[idx][0])], axis=0).astype(np.int64)
-    #     source_coord = np.dot(lin.inv(h_matrix), coord_homo) # 결과로 받은 source_coord는 [x, y] 순서임
-    #
-    #     # 사격형 그리기 -> scale 관련
-    #     for x, y, z in zip(source_coord[0,:], source_coord[1,:], ex[idx][2]):
-    #
-    #         # scale(시그마 말고)에 따른, 사각형의 중심 위치
-    #         x = x*(2**idx) + (2**idx-1)/2 # center X of Scale
-    #         y = y*(2**idx) + (2**idx-1)/2 # center Y of Scale
-    #         x_list.append(x)
-    #         y_list.append(y)
-    #
-    #         # 범위에 따른 사각형
-    #         rec = Rectangle((x-math.floor(6*sigmas[idx][z]/2), y-math.floor(6*sigmas[idx][z]/2)),
-    #                         math.floor(6*sigmas[idx][z]), math.floor(6*sigmas[idx][z]),
-    #                         linewidth=1,
-    #                         edgecolor='r',
-    #                         facecolor='none')
-    #         ax.add_patch(rec)
-    #     ax.scatter(x_list, y_list)
-    #
-    # plt.show()
+    fig, ax = plt.subplots()
+    ax.imshow(img, cmap='gray')
+
+    for idx in features.keys():
+        # if idx!=0 : continue
+        x_list = []
+        y_list = []
+
+        # deg rotation -> 그냥 로테이션 원상복귀만 시키는 파트 (scale과는 아무 관련 없음)
+        # X, Y, Z 순서 주의할 것 -> 영상은 기본적으로 (Height, Width, Channel) = (Y, X, Z) 순서임
+        shift = tran(-img.shape[1]/2, -img.shape[0]/2) # Homogeneous matrix는 x, y 순서로 받아야 한다.
+        shift_rev = tran(img.shape[1]/2, img.shape[0]/2)
+        rot_matrix = rot(deg)
+        h_matrix = np.linalg.multi_dot([shift_rev, rot_matrix, shift])
+
+        coord_homo = np.stack([localizedEx[idx][1],
+                               localizedEx[idx][0],
+                               np.ones_like(localizedEx[idx][0])], axis=0).astype(np.int64)
+        source_coord = np.dot(lin.inv(h_matrix), coord_homo) # 결과로 받은 source_coord는 [x, y] 순서임
+        # 사격형 그리기 -> scale 관련
+        for x, y, z in zip(source_coord[0,:], source_coord[1,:], localizedEx[idx][2]):
+
+            # scale(시그마 말고)에 따른, 사각형의 중심 위치
+            x = x*(2**idx) + (2**idx-1)/2 # center X of Scale
+            y = y*(2**idx) + (2**idx-1)/2 # center Y of Scale
+            x_list.append(x)
+            y_list.append(y)
+
+            # # 범위에 따른 사각형
+            # rec = Rectangle((x-math.floor(6*sigmas[idx][z]/2), y-math.floor(6*sigmas[idx][z]/2)),
+            #                 math.floor(6*sigmas[idx][z]), math.floor(6*sigmas[idx][z]),
+            #                 linewidth=1,
+            #                 edgecolor='r',
+            #                 facecolor='none')
+            # ax.add_patch(rec)
+        ax.scatter(x_list, y_list)
+
+    plt.show()
--- basic/filters.py
+++ basic/filters.py
@@ -13,7 +13,20 @@ def sobel(img):
 
     return imgDy, imgDx
 
-def sobelHeightAxis(img):
+def sobelFloat(img):
+    f_x = np.array([[-1, 0, 1],
+                    [-2, 0, 2],
+                    [-1, 0, 1]])
+    f_y = np.array([[-1, -2, -1],
+                    [ 0,  0,  0],
+                    [ 1,  2,  1]])
+    imgDx = cv2.filter2D(img, ddepth=cv2.CV_64F, kernel=f_x)
+    imgDy = cv2.filter2D(img, ddepth=cv2.CV_64F, kernel=f_y)
+    return imgDy, imgDx
+
+
+def sobelHeightAxis(img,
+                    ddepth=cv2.CV_16S):
     """
     해당 필터는 높이 방향으로만 처리되는 필터
     :param img:
@@ -22,10 +35,11 @@ def sobelHeightAxis(img):
     fHeight = np.array([[-1, -2, -1],
                         [ 0,  0,  0],
                         [ 1,  2,  1]])
-    imgDHeight = cv2.filter2D(img, ddepth=cv2.CV_16S, kernel=fHeight)
+    imgDHeight = cv2.filter2D(img, ddepth=ddepth, kernel=fHeight)
     return imgDHeight
 
-def sobelWidthAxis(img):
+def sobelWidthAxis(img,
+                   ddepth=cv2.CV_16S):
     """
     해당 필터는 너비 방향으로만 처리되는 필터
     :param img:
@@ -34,7 +48,7 @@ def sobelWidthAxis(img):
     fWidth = np.array([[-1, 0, 1],
                        [-2, 0, 2],
                        [-1, 0, 1]])
-    imgWidth = cv2.filter2D(img, ddepth=cv2.CV_16S, kernel=fWidth)
+    imgWidth = cv2.filter2D(img, ddepth=ddepth, kernel=fWidth)
     return imgWidth
 
 def gaussian(img,
