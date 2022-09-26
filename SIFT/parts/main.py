8ff6ef17bef6510142417fbe207ec268657e71c7
minpkang <minpkang@gmail.com>
2022-09-27 01:12:22 +0900

    SIFT BASIC FROM LOWE 1999, 2004 - 7. matching algorithm : FINISHED KDTREE

--- SIFT/parts/main.py
+++ SIFT/parts/main.py
@@ -6,9 +6,9 @@ from matching import *
 
 t1 = process_time()
 """STEP 1 : Loading an image"""
-imgDir = "D:\\cv\\data\\prac\\cannytest.png"
+imgDir = "D:\\cv\\data\\prac\\cup2.jpg"
 img = cv2.imread(imgDir, cv2.IMREAD_GRAYSCALE)  # shape order Y(height), X(width)
-img = cv2.resize(img, (250, 200))  # cv2.resize input shape order X(width), Y(height)
+img = cv2.resize(img, (400, 512))  # cv2.resize input shape order X(width), Y(height)
 
 """STEP 2 : Extracting key point from the image"""
 # STEP 2-1 : build scale space from the image
@@ -39,6 +39,47 @@ oriFeatures = orientation.assign(dogSpace=DoG,
                                  features=features)
 featureVect = orientation.featureVector(oriFeatures=oriFeatures,
                                         dogSpace=DoG)
+
+# visualize feature points
+img = img
+deg = 0
+fig, ax = plt.subplots()
+ax.imshow(img, cmap='gray')
+for idx in features.keys():
+    # if idx!=0 : continue
+    x_list = []
+    y_list = []
+
+    # deg rotation -> 그냥 로테이션 원상복귀만 시키는 파트 (scale과는 아무 관련 없음)
+    # X, Y, Z 순서 주의할 것 -> 영상은 기본적으로 (Height, Width, Channel) = (Y, X, Z) 순서임
+    shift = tran(-img.shape[1]/2, -img.shape[0]/2) # Homogeneous matrix는 x, y 순서로 받아야 한다.
+    shift_rev = tran(img.shape[1]/2, img.shape[0]/2)
+    rot_matrix = rot(deg)
+    h_matrix = np.linalg.multi_dot([shift_rev, rot_matrix, shift])
+
+    coord_homo = np.stack([features[idx][1],
+                           features[idx][0],
+                           np.ones_like(features[idx][0])], axis=0).astype(np.int64)
+    source_coord = np.dot(lin.inv(h_matrix), coord_homo) # 결과로 받은 source_coord는 [x, y] 순서임
+    # 사격형 그리기 -> scale 관련
+    for x, y, z in zip(source_coord[0,:], source_coord[1,:], features[idx][2]):
+
+        # scale(시그마 말고)에 따른, 사각형의 중심 위치
+        x = x*(2**idx) + (2**idx-1)/2 # center X of Scale
+        y = y*(2**idx) + (2**idx-1)/2 # center Y of Scale
+        x_list.append(x)
+        y_list.append(y)
+
+        # # 범위에 따른 사각형
+        # rec = Rectangle((x-math.floor(6*sigmas[idx][z]/2), y-math.floor(6*sigmas[idx][z]/2)),
+        #                 math.floor(6*sigmas[idx][z]), math.floor(6*sigmas[idx][z]),
+        #                 linewidth=1,
+        #                 edgecolor='r',
+        #                 facecolor='none')
+        # ax.add_patch(rec)
+    ax.scatter(x_list, y_list)
+
+plt.show()
 
 """STEP 4 : matching feature point"""
 root = KdTree.makeTree(featureVect)
