b59649a6fa1990e05dab7ef6cab4046a98543924
minpkang <minpkang@gmail.com>
2022-09-29 00:52:30 +0900

    SIFT BASIC FROM LOWE 1999, 2004 - 7. SIFT(without illum. normalization) init.

---
+++ SIFT/parts/testMain.py
@@ -0,0 +1,14 @@
+import cv2
+from main import dataBase
+
+# MAKE DATABASE
+imgDir1 = "D:\\cv\\data\\prac\\bcard.jpg"
+imgDir2 = "D:\\cv\\data\\prac\\bcard_background.jpg"
+
+dataBase1 = dataBase(imgDir=imgDir1)
+dataBase2 = dataBase(imgDir=imgDir2)
+
+# COMPARE / MATCHING
+
+
+
